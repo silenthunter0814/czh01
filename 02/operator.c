08c077d65ae81b7d77ded314360f02135146b1af
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-04-13 20:48:43 +0800

    Create operator.c

---
+++ 02/operator.c
@@ -0,0 +1,16 @@
+/* 
+ * file name: oprator.c
+ */
+
+#include <stdio.h>
+
+int main()
+{
+        int x, i;
+
+        i = 3;
+        x = 5;
+        i *= x +6;
+        printf("i = %d\n", i);
+        return 0;
+}
