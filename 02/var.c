ced676b40c31e7ca8a385ad714b3b9edaf5d4330
Silent Hunter <121765771+silenthunter0814@users.noreply.github.com>
2023-04-13 17:54:18 +0800

    Create var.c

---
+++ 02/var.c
@@ -0,0 +1,18 @@
+/* 
+ * file name: var.c
+ */
+
+#include <stdio.h>
+
+int main()
+{
+        int x;
+        int y;
+
+        x = 28;
+        y = 3523;
+        printf("x = %d\n", x);
+        printf("y = %d\n", y);
+        printf("x = %d\ty=%d\n", x, y);
+        return 0;
+}
